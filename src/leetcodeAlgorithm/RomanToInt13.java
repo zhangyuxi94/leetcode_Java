a5d20678079fdc991c49fa1be6bc61bfabe0ba4b
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-09-20 11:31:39 -0400

    add leetcode Java

---
+++ src/leetcodeAlgorithm/RomanToInt13.java
@@ -0,0 +1,5 @@
+package leetcodeAlgorithm;
+
+public class RomanToInt13 {
+
+}
---
+++ src/leetcodeAlgorithm/StringToInt.java
@@ -0,0 +1,49 @@
+package leetcodeAlgorithm;
+
+
+
+public class StringToInt {
+	public int myAtoi(String str){
+		int result=0;
+		int neg=1;
+		int count=0;
+		char[] charArr=str.toCharArray();
+		for(int i=0;i<charArr.length;i++){
+			count++;
+			if(charArr[i]>='0'&&charArr[i]<='9'){
+				result*=10;
+				result+=(charArr[i]-'0');
+			}
+			else if(charArr[i]=='-'&&count==1){
+				neg=-1;
+				count--;
+			}
+			else if(charArr[i]=='+'&&count==1){
+				neg=1;
+				count--;
+			}
+			else if(charArr[i]==' '&&count==1){
+				count--;
+			}
+			else{
+				break;
+			}
+		}
+		
+		if(result>Integer.MAX_VALUE){
+			if(neg==1){
+				return Integer.MAX_VALUE;
+			}
+			else{
+				return Integer.MIN_VALUE;
+			}
+			
+		}
+		return (neg==1)?result:neg*result;
+	}
+	public static void main(String[] args) {
+		StringToInt test=new StringToInt();
+		System.out.println(test.myAtoi(" 2341"));
+	}
+}
+
