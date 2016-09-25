575c7641bf90113d9ab669d3c22e6e57055ce0ec
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-09-25 17:03:48 -0400

    updated firstUniqChar

--- src/leetcodeAlgorithm/FirstUniqueCharInStr387.java
+++ src/leetcodeAlgorithm/FirstUniqueCharInStr387.java
@@ -5,27 +5,37 @@ public class FirstUniqueCharInStr387 {
 		if(str.length()==0){
 			return -1;
 		}
-		else if(str.length()==1){
+		if(str.length()==1){
 			return 0;
 		}
-		char[] charArr=str.toCharArray();
-		int[] count=new int[128];
+		if(str.length()!=0){
+			char[] charArr=str.toCharArray();
+			int[] count=new int[128];
+			int result=str.length()-1;
 		
-		for(int i=0;i<charArr.length;i++){
-			int asciiVal=(int)charArr[i];
-			count[asciiVal]++;
+			for(int i=0;i<charArr.length;i++){
+				int asciiVal=(int)charArr[i];
+				count[asciiVal]++;
+				if(count[asciiVal]==charArr.length){
+					return -1;
+				}
+			}
+			int test=0;
+			for(int j=0;j<count.length;j++){
+				if(count[j]==1){
+					int number=str.indexOf((char)j);
+					result=Math.min(result,number);
+					test++;
+				}	
+			}
+			if(test==0){
+				return -1;
+			}
+			return result;
 		}
-		int result=0;
-		for(int j=0;j<count.length;j++){
-			if(count[j]==1){
-				result=j;
-				result=Math.min(result,j);
-			}	
-		}
-		int n=str.indexOf((char)result);
-		return n;
+		return -1;
 	}
 	public static void main(String[] args) {
-		System.out.println(findFirstUni("cabfabcd"));
+		System.out.println(findFirstUni("cccbb"));
 	}
 }
