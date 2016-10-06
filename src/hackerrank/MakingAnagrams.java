aa7eb816e8ab736c2bf4f07e1b0645dedea134b4
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-10-06 19:52:24 -0400

    added Making Anagrams

---
+++ src/hackerrank/MakingAnagrams.java
@@ -0,0 +1,42 @@
+package hackerrank;
+
+import java.util.Arrays;
+
+public class MakingAnagrams {
+	 public static int numberNeeded(String first, String second) {
+		 char[] chArr1=first.toCharArray();
+		 char[] chArr2=second.toCharArray();
+//		 chArr1
+		 Arrays.sort(chArr1);
+		 Arrays.sort(chArr2);
+		
+		 int loopLen=Math.min(chArr1.length, chArr2.length);
+		 int i=0,j=0,dele=0;
+		 while((i<chArr1.length)&&(j<chArr2.length)){
+			 if(chArr1[i]>chArr2[j]){
+				 j++;
+				 dele++;
+			 }
+			 else if(chArr1[i]==chArr2[j]){
+				 i++;
+				 j++;
+			 }
+			 else if(chArr1[i]<chArr2[j]){
+				 i++;
+				 dele++;
+			 }
+		 }
+		 if(i==chArr1.length){
+			 dele+=chArr2.length-j;
+		 }
+		 if(j==chArr2.length){
+			 dele+=chArr1.length-i;
+		 }
+		 return dele;
+	 }
+	public static void main(String[] args) {
+		String first="cde";
+		String second="abc";
+		System.out.println(numberNeeded(first, second));
+	}
+}
--- src/leetcodeAlgorithm/GroupAnagrams49.java
+++ src/leetcodeAlgorithm/GroupAnagrams49.java
@@ -3,7 +3,6 @@ package leetcodeAlgorithm;
 import java.util.*;
 
 public class GroupAnagrams49 {
-
 	  public static List<List<String>> groupAnagrams(String[] strs) {
 		  HashMap<String,List<String>> map=new HashMap<String,List<String>>();
 		  List<List<String>> result=new ArrayList<List<String>>(5);
@@ -18,8 +17,7 @@ public class GroupAnagrams49 {
 		  }
 		for(List<String> ls:map.values()){
 			result.add(ls);
-		}
-		  
+		}	  
 		  return result;
 	    }
 	public static void main(String[] args) {
