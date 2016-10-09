48474e8033dcbc36c5a4470d4836ad9774df6f9f
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-10-09 17:13:48 -0400

    added rectangle area

---
+++ src/leetcodeAlgorithm/RectangleArea223.java
@@ -0,0 +1,22 @@
+package leetcodeAlgorithm;
+
+public class RectangleArea223 {
+	  public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+	      int leftArea=(C-A)*(D-B);
+	      int rightArea=(G-E)*(H-F);
+	      int overlap=0;
+	      int top=Math.min(D, H);
+	      int right=Math.min(C, G);
+	      int btm=Math.max(B,F);
+	      int left=Math.max(E, A);
+	      if(right>left&&top>btm){
+	    	  overlap=(right-left)*(top-btm);
+	      }
+		  return leftArea+rightArea-overlap;
+	    }
+	public static void main(String[] args) {
+		System.out.println(computeArea(-3,0,3,4,0,-1,9,2));
+
+	}
+
+}
