2207ca9b97703108b48a70be6ee4660f100dc078
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2016-10-08 23:51:46 -0400

    updated excel column

---
+++ src/leetcodeAlgorithm/ExcelColumn168.java
@@ -0,0 +1,18 @@
+package leetcodeAlgorithm;
+
+public class ExcelColumn168 {
+	public static String convertToTitle(int n) {
+		String res="";
+		while(n!=0){
+			char ch=(char)((n-1)%26+65);
+			n=(n-1)/26;
+			res=ch+res;
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		System.out.println(convertToTitle(22));
+		
+	}
+
+}
